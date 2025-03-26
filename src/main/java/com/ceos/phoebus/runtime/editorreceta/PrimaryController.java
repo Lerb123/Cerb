77dfd5ed407a584fa5b4fd133b6d34be2d4910b7
Lerb123 <luis.rodriguez@ceos.com.ve>
2025-03-26 12:47:24 -0400

    add items to comboBox products

--- src/main/java/com/ceos/phoebus/runtime/editorreceta/PrimaryController.java
+++ src/main/java/com/ceos/phoebus/runtime/editorreceta/PrimaryController.java
@@ -7,10 +7,10 @@ package com.ceos.phoebus.runtime.editorreceta;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -18,22 +18,22 @@ import javafx.scene.control.ComboBox;
  * @author ceos
  */
 public class PrimaryController implements Initializable {
-
-    ObservableList<String> products = FXCollections.observableArrayList("Frescolita 2L", "Chinotto 2L",
-            "Chinotto 1.5L", "Coca-Cola 1.5L", "Producto Sc", "Coca-Cola Light 2L",
-            "Coca-Cola 2L", "Grapette Naranja 1.5", "CocaCola m-c1.25L", "CocaCola 350ml",
-            "Grapette Uva 1.5L");
     /**
      * Initializes the controller class.
      */
-    
-    @FXML
-    private ComboBox productsBox;
-    
+
+    @FXML private ComboBox <String> miComboBox;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        productsBox.setValue("Coca-Cola 2L");
-        productsBox.setItems(products);
-    }    
+//        miComboBox.setItems(FXCollections.observableArrayList("Frescolita 2L", "Chinotto 2L",
+//            "Chinotto 1.5L", "Coca-Cola 1.5L", "Producto Sc", "Coca-Cola Light 2L",
+//            "Coca-Cola 2L", "Grapette Naranja 1.5", "CocaCola m-c1.25L", "CocaCola 350ml",
+//            "Grapette Uva 1.5L"));
+
+
+    }
     
+  
+
 }
