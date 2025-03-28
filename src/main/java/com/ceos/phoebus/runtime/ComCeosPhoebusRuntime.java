d143de4e4027555ae9e0349632aafa53560befe7
Lerb123 <luis.rodriguez@ceos.com.ve>
2025-03-28 19:13:59 -0400

    Add database

--- src/main/java/com/ceos/phoebus/runtime/ComCeosPhoebusRuntime.java
+++ src/main/java/com/ceos/phoebus/runtime/ComCeosPhoebusRuntime.java
@@ -4,6 +4,10 @@
 
 package com.ceos.phoebus.runtime;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  *
  * @author ceos
@@ -11,7 +15,41 @@ package com.ceos.phoebus.runtime;
 public class ComCeosPhoebusRuntime {
 
     public static void main(String[] args) {
-    //   CeosUtils.initNumberBoardScene();
-    
+        Connection con = CeosUtils.getConnection();
+        String seleccion = "Hit Uva Sin Calorías 2L";
+     try {
+            String query = "SELECT * FROM receta WHERE nameproduct='"+seleccion+"';";
+            Statement ps = con.createStatement();
+            ResultSet rs = ps.executeQuery(query);
+            System.out.println(rs);
+            while (rs.next()) {
+                double syrupBrix=0, drinkBrix=0;
+                if (rs.getString("type").equalsIgnoreCase("brix")) {
+                    syrupBrix = rs.getDouble("syrupbrix");
+                    drinkBrix = rs.getDouble("drinkbrix");
+                }
+                int recipeNumber = rs.getInt("recipenumber");
+                
+                double specificWeightSyrup = rs.getDouble("specificweightsyrup");
+                
+                double nominalRatio = rs.getDouble("nominalratio");
+                double co2Volume = rs.getDouble("co2volume");
+                double saturatorPressure = rs.getDouble("saturatorpressure");
+                double temperatureDrinking = rs.getDouble("temperaturedrinking");
+                double nominalProduction = rs.getDouble("nominalproduction");
+                double minLevel = rs.getDouble("minLevel");
+                int vm1 = rs.getInt("vm1");
+                int vm2 = rs.getInt("vm2");
+                int vm3 = rs.getInt("vm3");
+                
+                
+                //asignar valores
+                
+            }
+            rs.close();
+            ps.close();
+            con.close();
+        } catch (Exception e) {
+        }
     }
 }
--- src/main/java/com/ceos/phoebus/runtime/editorreceta/PrimaryController.java
+++ src/main/java/com/ceos/phoebus/runtime/editorreceta/PrimaryController.java
@@ -4,7 +4,12 @@
  */
 package com.ceos.phoebus.runtime.editorreceta;
 
+import com.ceos.phoebus.runtime.CeosUtils;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -12,6 +17,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -35,10 +41,95 @@ public class PrimaryController implements Initializable {
     private CheckBox diet;
 
     @FXML
+    private TextField numberRecipe;
+    @FXML
+    private TextField brixsyrup;
+    @FXML
+    private TextField specificWeight;
+    @FXML
+    private TextField brixBeverage;
+    @FXML
+    private TextField nominalRatio;
+    @FXML
+    private TextField co2Volumes;
+    @FXML
+    private TextField saturatorPressure;
+    @FXML
+    private TextField beverageTemperature;
+    @FXML
+    private TextField nominalProduction;
+    @FXML
+    private TextField minNivel;
+    @FXML
+    private Label vm1;
+    @FXML
+    private Label vm2;
+    @FXML
+    private Label vm3;
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        miComboBox.setValue("Coca-Cola Menos Calorías 2L");
+    }
+
+    @FXML
     public void handleValueAction() {
-        String seleccion = miComboBox.getValue();
-        actualRecipe.setText(seleccion);
+        double syrupBrix = 0, drinkBrix = 0;
+        int recipeNumber, vm_1, vm_2, vm_3;
+        double specificWeightSyrup, nominalratio, co2Volume, saturatorpressure, temperatureDrinking, nominalproduction, minLevel;
+        //activa y llena los datos de la receta en los fields
+        //consulta de seleecion partiendo del valor actual del combobox
+        Connection con = CeosUtils.getConnection();
+        System.out.println("voy aqui");
+        try {
+
+            String seleccion = miComboBox.getValue();
+            String query = "SELECT * FROM receta WHERE nameproduct='" + seleccion + "';";
+            Statement ps = con.createStatement();
+            ResultSet rs = ps.executeQuery(query);
+            while (rs.next()) {
+
+                if (rs.getString("type").equalsIgnoreCase("brix")) {
+                    syrupBrix = rs.getDouble("syrupbrix");
+                    drinkBrix = rs.getDouble("drinkbrix");
+                }
+                recipeNumber = rs.getInt("recipenumber");
+
+                specificWeightSyrup = rs.getDouble("specificweightsyrup");
+
+                nominalratio = rs.getDouble("nominalratio");
+                co2Volume = rs.getDouble("co2volume");
+                saturatorpressure = rs.getDouble("saturatorpressure");
+                temperatureDrinking = rs.getDouble("temperaturedrinking");
+                nominalproduction = rs.getDouble("nominalproduction");
+                minLevel = rs.getDouble("minLevel");
+                vm_1 = rs.getInt("vm1");
+                vm_2 = rs.getInt("vm2");
+                vm_3 = rs.getInt("vm3");
+
+                System.out.println(co2Volume);
+                //asignar valores
+                numberRecipe.setText(String.valueOf(recipeNumber));
+                brixsyrup.setText(String.valueOf(syrupBrix));
+                specificWeight.setText(String.valueOf(specificWeightSyrup));
+                brixBeverage.setText(String.valueOf(drinkBrix));
+                nominalRatio.setText(String.valueOf(nominalratio));
+                co2Volumes.setText(String.valueOf(co2Volume));
+                saturatorPressure.setText(String.valueOf(saturatorpressure));
+                beverageTemperature.setText(String.valueOf(temperatureDrinking));
+                nominalProduction.setText(String.valueOf(nominalproduction));
+                minNivel.setText(String.valueOf(minLevel));
+                vm1.setText(String.valueOf(vm_1));
+                vm2.setText(String.valueOf(vm_2));
+                vm3.setText(String.valueOf(vm_3));
+            }
+            rs.close();
+            ps.close();
+            con.close();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
     @FXML
@@ -62,19 +153,10 @@ public class PrimaryController implements Initializable {
     }
 
     @FXML
-    public void handleProportion() {
-        if (proportion.isSelected()) {
-            brix.setSelected(false);
-            diet.setSelected(false);
-
-            //more logic
-        }
-    }
-
-    @FXML
     public void handleBrix() {
+        brixsyrup.setVisible(true);
+        brixBeverage.setVisible(true);
         if (brix.isSelected()) {
-            proportion.setSelected(false);
             diet.setSelected(false);
 
             //more logic
@@ -84,16 +166,12 @@ public class PrimaryController implements Initializable {
     @FXML
     private void handleDiet() {
         if (diet.isSelected()) {
-            proportion.setSelected(false);
             brix.setSelected(false);
 
             //more logic
+            brixsyrup.setVisible(false);
+            brixBeverage.setVisible(false);
         }
-    }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-
     }
 
     @FXML
